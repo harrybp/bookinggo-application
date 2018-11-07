80d5995ad8294c2f21022bb39a12257748da02b2
Harry <harrytbp@gmail.com>
2018-11-07 18:57:29 +0000

    Extended cli to cope with a timeout or lack of response from one or more of the APIs

--- cli.js
+++ cli.js
@@ -31,51 +31,77 @@ try {
 let axios = require('axios');
 const apiRoot = 'https://techtest.rideways.com/'
 let queries = '?pickup=' + pickup + '&dropoff=' + dropoff
-axios.all([
-  //Make a request for each supplier
-  axios.get(apiRoot + 'dave' + queries),
-  axios.get(apiRoot + 'eric' + queries),
-  axios.get(apiRoot + 'jeff' + queries)
-])
-  .then(response => {
-    let vendors = ['dave', 'eric', 'jeff']
-    var filtered = []; //Filter the responses so only the cheapest for each vehicle type remains
-    for(var v in vendors){ //Loop over dave,eric,jeff
-      let options = response[v].data.options;
-      for(var o in options){ //Loop over options returned for this vendor
-        let type = options[o].car_type
-        let price = options[o].price
-        //Check if this vehicle type is already in the filtered list
-        let found = -1;
-        for(var i in filtered){
-          if(filtered[i].type == type){
-            found = i;
-            break;
-          }
-        }
-        if(found >= 0){ //If entry was already in list
-          if(filtered[found].price > price){ //Overwrite entry if the price is lower
-            filtered[found].type = type;
-            filtered[found].price = price;
-            filtered[found].vendor = vendors[v];
-          }
-        } else { //Entry was not in filtered list so add it to list
-          filtered.push({'type':type,'price':price, 'vendor': vendors[v]});
-        }
-      }
-    }
-    filtered.sort(function(a, b){
-      return a.price - b.price;
-    })
-    for(var i in filtered){
-      if(passengerCapacity[filtered[i].type] >= noPassengers){
-        //Only print if vehicle capacity is sufficient
-        console.log(filtered[i].type + '-' + filtered[i].vendor + '-' + filtered[i].price)
-        responded = true
-      }
-    }
-    if(!responded) console.log('No suitable vehicles found')
+let filtered =[]
+callDave(filtered);
+
+//Calls DAVE API, then calls callEric method
+function callDave(filtered){
+  axios({ method: 'GET', url: apiRoot + 'dave' + queries, timeout: 2000
+  }).then(response => { callEric(addToList(filtered, response.data.options, 'dave'));
   })
   .catch(error => {
-    console.log('API error, please try again (' + error + ')');
-  });
+    console.log('> API for DAVE did not respond or was too slow');
+    callEric(filtered);
+  })
+}
+//Calls ERIC API, then calls callJeff method
+function callEric(filtered){
+  axios({ method: 'GET', url: apiRoot + 'eric' + queries, timeout: 2000
+  }).then(response => { callJeff(addToList(filtered, response.data.options, 'eric'));
+  })
+  .catch(error => {
+    console.log('> API for ERIC did not respond or was too slow');
+    callJeff(filtered);
+  })
+}
+//Calls JEFF API, then calls accumulate method
+function callJeff(filtered){
+  axios({ method: 'GET', url: apiRoot + 'jeff' + queries, timeout: 2000
+}).then(response => { accumulate(addToList(filtered, response.data.options, 'jeff'));
+  })
+  .catch(error => {
+    console.log('> API for JEFF did not respond or was too slow');
+    accumulate(filtered);
+  })
+}
+//Sorts and prints the filtered list of taxi options
+function accumulate(filtered){
+  filtered.sort(function(a, b){
+    return a.price - b.price;
+  })
+  for(var i in filtered){
+    if(passengerCapacity[filtered[i].type] >= noPassengers){
+      //Only print if vehicle capacity is sufficient
+      console.log(filtered[i].type + '-' + filtered[i].vendor + '-' + filtered[i].price)
+      responded = true
+    }
+  }
+  if(!responded) console.log('No suitable vehicles found')
+}
+
+
+//Appends the response from one api call to the filtered list
+function addToList(list, response, vendor){
+  for(var o in response){ //Loop over options returned for this vendor
+    let type = response[o].car_type
+    let price = response[o].price
+    //Check if this vehicle type is already in the filtered list
+    let found = -1;
+    for(var i in list){
+      if(list[i].type == type){
+        found = i;
+        break;
+      }
+    }
+    if(found >= 0){ //If entry was already in list
+      if(list[found].price > price){ //Overwrite entry if the price is lower
+        list[found].type = type;
+        list[found].price = price;
+        list[found].vendor = vendor;
+      }
+    } else { //Entry was not in filtered list so add it to list
+      list.push({'type':type,'price':price, 'vendor': vendor});
+    }
+  }
+  return list;
+}
